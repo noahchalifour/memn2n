35750549b9973dfc72b58412d17358a5efebe8d7
Noah Chalifour <chalifournoah@gmail.com>
2020-01-29 18:03:53 -0500

    Modified hparams

--- utils/hparams.py
+++ utils/hparams.py
@@ -4,8 +4,8 @@ HP_TOKEN_TYPE = hp.HParam('token_type', hp.Discrete(['word']))
 HP_VOCAB_SIZE = hp.HParam('vocab_size', hp.Discrete([-1]))
 HP_EMBED_SIZE = hp.HParam('embedding_size', hp.Discrete([32, 128]))
 
-HP_MEMORY_SIZE = hp.HParam('memory_size', hp.Discrete([50, 100, 250]))
-HP_MEMORY_HOPS = hp.HParam('memory_hops', hp.Discrete([1, 3, 5, 10]))
+HP_MEMORY_SIZE = hp.HParam('memory_size', hp.Discrete([50, 100]))
+HP_MEMORY_HOPS = hp.HParam('memory_hops', hp.Discrete([1, 3, 5]))
 
 HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([32]))
 HP_EPOCHS = hp.HParam('epochs', hp.Discrete([200]))
